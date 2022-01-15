cd493677358939f63d3f7b2499c5b6dd2f2d2255
Amr Mahmoud Morsy <ammorsy@MOBCORP.INTRT.COM>
2022-01-15 19:27:39 +0200

    change server file

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/cairo-south'));
+app.use(express.static('./dist/CairoSouth'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/cairo-south/'}),
+    res.sendFile('index.html', {root: 'dist/CairoSouth/'}),
 );
 
 // Start the app by listening on the default Heroku port
